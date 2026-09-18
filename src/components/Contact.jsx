f2417d02469870941b547f368a0a5796edc624ad
vishnu-c-2026 <vishnu.c@decirclesolar.com>
2026-09-18 16:59:27 +0530

    linked in icon and removeing contact info from ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -152,8 +152,8 @@ export default function Contact() {
               </button>
             ))}
           </div>
-
-          <div className="c-block">
+  
+          {/* <div className="c-block">
             <h4>Email</h4>
             <p>
               <a href="mailto:care@decirclesolar.com">care@decirclesolar.com</a>
@@ -166,7 +166,7 @@ export default function Contact() {
           <div className="c-block">
             <h4>Regions Served</h4>
             <p>UAE · India</p>
-          </div>
+          </div> */}
           <div className="c-block">
             <h4>Company Profile</h4>
             <a
@@ -181,14 +181,17 @@ export default function Contact() {
             href="https://www.linkedin.com/company/decircle-solar-dwc-llc/"
             target="_blank"
             rel="noopener noreferrer"
-            style={{
-              fontFamily: "'Montserrat',sans-serif",
-              fontWeight: 600,
-              fontSize: "14.5px",
-              color: "var(--orange)",
-            }}
+            aria-label="LinkedIn"
           >
-            LinkedIn ↗
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="var(--orange)"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M20.45 20.45h-3.56v-5.57c0-1.33-.02-3.04-1.85-3.04-1.85 0-2.14 1.45-2.14 2.94v5.67H9.34V9h3.42v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.6 0 4.27 2.37 4.27 5.46v6.28zM5.34 7.43a2.06 2.06 0 1 1 0-4.13 2.06 2.06 0 0 1 0 4.13zM7.12 20.45H3.56V9h3.56v11.45zM22.22 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.45c.98 0 1.78-.77 1.78-1.72V1.72C24 .77 23.2 0 22.22 0z" />
+            </svg>
           </a>
         </div>
         <div>
